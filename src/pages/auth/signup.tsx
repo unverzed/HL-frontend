dd43aa98a5ac900f892f649e6f01e7c9f04637fb
unverzed <vitoriarortega@gmail.com>
2022-11-14 03:05:08 -0300

    feat: creating a user in signup

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,15 +1,73 @@
 import { Form, Title, Main, StyledLink } from "./style";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const URL = `http://localhost:5000`;
+
+  function createUser(event: any) {
+    event.preventDefault();
+    setLoading(true);
+
+    const data = { name, email, password };
+    const promise = axios.post(`${URL}/signup`, data);
+    promise.then((response) => {
+      console.log(response);
+      setName("");
+      setEmail("");
+      setPassword("");
+    });
+
+    promise.catch((error) => {
+      console.log(error);
+      setLoading(false);
+    });
+  }
+
   return (
     <Main>
-      <Title>HubLocal</Title>
-      <Form>
-        <input type="text" required placeholder="Nome"></input>
-        <input type="email" required placeholder="Email"></input>
-        <input type="password" required placeholder="Senha"></input>
-        <button type="submit">Cadastrar</button>
+      <Title>
+        HubLocal
+      </Title>
+      <Form onSubmit={createUser}>
+        <input
+          type="text"
+          disabled={loading}
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Nome"
+        ></input>
+        <input
+          type="email"
+          disabled={loading}
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        ></input>
+        <input
+          type="password"
+          disabled={loading}
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Senha"
+        ></input>
+        <button type="submit" disabled={loading}>
+          {loading ? (
+            <div className="loading">
+              <h1>Loading...</h1>
+            </div>
+          ) : (
+            "Signup"
+          )}
+        </button>
         <StyledLink>
           Já tem uma conta?
           <Link to="/">
